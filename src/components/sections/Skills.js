b19aba653cccc9909a727c7bc15d91985d69aafa
Bhushan Patil <thebhushanp@gmail.com>
2020-05-29 17:49:13 +0530

    Update Skills.js

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -8,6 +8,22 @@ function Skills() {
       <div className="container">
         <h1 className="title">Skills</h1>
         <div className="columns">
+            <div className="column is-6">
+            <div className="has-text-centered">
+              <span className="icon has-text-link">
+                <i className="fas fa-3x fa-laptop-code"></i>
+              </span>
+              <h2 className="title is-5">Software Development</h2>
+            </div>
+            <SkillsList
+              skills={Resume.skills
+                .filter(skill => skill.keywords.includes("Software Developer"))
+                .reduce((obj, item) => {
+                  obj[item.name] = item.level;
+                  return obj;
+                }, {})}
+            />
+          </div>
           <div className="column is-6">
             <div className="has-text-centered">
               <span className="icon has-text-link">
@@ -24,22 +40,7 @@ function Skills() {
                 }, {})}
             />
           </div>
-          <div className="column is-6">
-            <div className="has-text-centered">
-              <span className="icon has-text-link">
-                <i className="fas fa-3x fa-laptop-code"></i>
-              </span>
-              <h2 className="title is-5">Software Development</h2>
-            </div>
-            <SkillsList
-              skills={Resume.skills
-                .filter(skill => skill.keywords.includes("Software Developer"))
-                .reduce((obj, item) => {
-                  obj[item.name] = item.level;
-                  return obj;
-                }, {})}
-            />
-          </div>
+        
         </div>
       </div>
     </section>
